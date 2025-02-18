9305411d6013d6ea770b06ffcacd6aadc715f406
Håkan Haraldsson <hakan.haraldsson@ingka.com>
2025-02-18 20:08:04 +0100

    Update copyright year (#1)

--- src/batch.js
+++ src/batch.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 Ingka Holding B.V. All Rights Reserved.
+ * Copyright (c) 2025 Ingka Holding B.V. All Rights Reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
